18d7ca8f6a4ea9e8565334363edc744ea04a7a51
Robert Sherman <rsherman@itworksglobal.com>
2016-08-23 00:07:09 -0400

    searchable cards SPLASH SCREEN

--- www/js/index.js
+++ www/js/index.js
@@ -35,16 +35,33 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		$("#srchBtnA").click(function() {
+			app.searchCards();
+		});
+		$('#splashCover').css("height","100%").css("width","100%")
+			.css("min-height","100%").css("min-width","100%")
+			.css("z-index","1000").css("display","block");
+		$('#splashCover').html("<br/><br/><br/><br/><h1 style'color:white'><strong>READY</strong></h3>" + 
+		"<br/><br/><br/><br/><strong>Loading All Cards</strong>");
+		
 		// load the big ass JSON data sets
 		// use AJAX - cordova thinks this is a website
 		// puny cordova
 		var path="db/";
 		$.ajax({ url: path + "AllCards-x.json" })
                 .done(function(json) {
+						$('#splashCover').html("<br/><br/><br/><br/><h2 style'color:white'><strong>SET</strong></h3>" + 
+		"<br/><br/><br/><br/><strong>Loading All Sets</strong>");
 						app.db.allCards = $.parseJSON(json);
 						$.ajax({ url: path + "AllSets-x.json" })
 								.done(function(json) {
+										$('#splashCover').html("<br/><br/><br/><br/><h3 style'color:white'><strong>GO</strong></h3>" + 
+		"<br/><br/><br/><br/><strong>Parsing data</strong>");
 										app.db.allSets = $.parseJSON(json);
+											setTimeout(function() { 
+												$('#splashCover').css("display","none");
+												$('#splashCover').html("");
+											}, 2000);
 									});
 						});
 		
@@ -72,7 +89,59 @@ var app = {
 			}
 		}
 		return objects;
+	},
+	searchCards: function() {
+		$("#tempResultDisplay").html("");
+		
+		var srch4thisName = $('#txtSrchName').val();
+		var objects = [];
+		for(var card in app.db.allCards) {
+			if (card==srch4thisName) objects.push(app.db.allCards[card]);
+		}
+		var count = objects.length;
+		if (count===0) return;
+		var T = $("<table></table>")
+				.css("width","100%").css("padding", "3px").css("border-spacing","0")
+				.css("border-collapse","collapse");
+		for (idx=0; idx<count;idx++) {
+			var foundCard = objects[idx];
+			Object.getOwnPropertyNames(foundCard).forEach(function(val, idx, array) {
+				var R = $('<tr style="border-bottom:1px solid blue"></tr>');
+			   	$("<td>" + val + "</td>").appendTo(R);
+				if (typeof(foundCard[val])!="object") {
+					// simple value
+					$("<td>" + foundCard[val]+"</td>").appendTo(R);
+				} else {
+					// object value
+					var tmp = "<td>";
+					Object.getOwnPropertyNames(foundCard[val])
+						.forEach(function(vl, id, ary) {
+							if (typeof(foundCard[val][vl])=="object" && val=="legalities") {
+								Object.getOwnPropertyNames(foundCard[val][vl])
+									.forEach(function(v, i, ar) {
+										tmp+="<br>" + v + " : " + foundCard[val][vl][v];
+									});
+							} else {
+								if (vl!="length") {
+									if (parseInt(vl)>=0 || parseInt(vl)<=100) {
+										tmp+="<br>" + foundCard[val][vl];
+									} else {
+										tmp+="<br>" + vl + "&nbsp;" + foundCard[val][vl];
+									}
+								}
+							}
+						});
+					tmp+="</td>";
+					$(tmp).appendTo(R);
+				}
+				$(R).appendTo(T);
+			 });
+			 
+		}
+		
+		$(T).appendTo("#tempResultDisplay");
+		$(T).trigger('create');
 	}
-};
+}
 
 app.initialize();
